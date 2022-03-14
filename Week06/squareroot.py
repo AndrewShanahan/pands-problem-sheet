de923ca10d7611c92027779072f58cd892a5edff
Andrew Shanahan <andrewshanahan3@gmail.com>
2022-03-14 17:47:07 +0000

    Week06

--- Week06/squareroot.py
+++ Week06/squareroot.py
@@ -3,9 +3,9 @@
 # Reference 1: https://www.w3schools.com/python/python_functions.asp
 # Reference 2: https://en.wikipedia.org/wiki/Newton%27s_method#Square_root_of_a_number
 # Reference 3: https://stackoverflow.com/questions/1623375/writing-your-own-square-root-function
-#def sqrt(t):
-   # r = t/2.0; t = float(t)
- #   i = 0
-  #  while i &lt; 10:
-          #  r = (r+t/r)/2.0
-          #  i += 1
+def sqrt(t):
+  # r = t/2.0; t = float(t)
+    i = 0
+    while i &lt; 10:
+            r = (r+t/r)/2.0
+            i += 1
