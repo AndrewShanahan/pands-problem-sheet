d055c04c83308f3b3e97dbea89e5e4f974b3ee35
Andrew Shanahan <andrewshanahan3@gmail.com>
2022-05-07 12:23:06 +0100

    Update to collatz.py, still WIP

--- collatz.py
+++ collatz.py
@@ -1,2 +1,22 @@
-# 
 # Author: Andrew Shanahan
+# Write a program that asks the user to input any positive integer and outputs the successive values of the following calculation.
+# At each step calculate the next value by taking the current value and, if it is even, divide it by two, but if it is odd, multiply it by three and add one.
+# Have the program end if the current value is one.
+
+Input = int(input("Enter any value:
+"))
+Positive = abs(Input)
+
+# or
+
+while True:
+            try:
+                number =
+int(input("Enter Number: "))
+                    assert(number >0),
+'Number must be bigger than 0'
+                break
+            except:
+                    print("This is a sting")
+                    
+
